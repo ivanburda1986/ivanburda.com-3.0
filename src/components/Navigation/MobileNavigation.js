ef95f6c3222589b6e14f9939a735bd87809335b1
ivanburda1986 <burda.ivan@gmail.com>
2021-09-19 17:13:24 +0200

    finished the mobile nav

--- src/components/Navigation/MobileNavigation.js
+++ src/components/Navigation/MobileNavigation.js
@@ -4,29 +4,31 @@ import { FiMenu } from "react-icons/fi";
 import styles from "./MobileNavigation.module.css";
 
 export default function MobileNavigation() {
+  const [showNavigation, setShowNavigation] = React.useState(true);
+
   return (
     <div className={styles.MobileNavigation}>
       <div className={styles.container}>
         <div className={styles.MobileNavigationHeader}>
           <h1>Ivan Burda</h1>
-          <button className={styles.hamburger}>
+          <button className={styles.hamburger} onClick={() => setShowNavigation(!showNavigation)}>
             <FiMenu />
           </button>
         </div>
-        <div className={styles.MobileNavigationList}>
+        <div className={styles.MobileNavigationList} style={{ display: `${showNavigation === true ? "none" : "flex"}` }}>
           <ul>
             <li className={styles.menuOption}>
-              <Link to="about" smooth={true} spy={true} activeClass={styles.active}>
+              <Link to="about" smooth={true} spy={true} activeClass={styles.active} onClick={() => setShowNavigation(!showNavigation)}>
                 About
               </Link>
             </li>
             <li className={styles.menuOption}>
-              <Link to="experience" smooth={true} spy={true} activeClass={styles.active}>
+              <Link to="experience" smooth={true} spy={true} activeClass={styles.active} onClick={() => setShowNavigation(!showNavigation)}>
                 Experience
               </Link>
             </li>
             <li className={styles.menuOption}>
-              <Link to="devportfolio" smooth={true} spy={true} activeClass={styles.active}>
+              <Link to="devportfolio" smooth={true} spy={true} activeClass={styles.active} onClick={() => setShowNavigation(!showNavigation)}>
                 Dev portfolio
               </Link>
             </li>
